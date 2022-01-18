01a0883492f99e5a6d887e2e09efc34f4f605ba3
Miguel Rivadeneira <migeriva@espol.edu.ec>
2022-01-18 10:21:04 -0500

    Limpieza

--- backend/README.md
+++
@@ -1,6 +0,0 @@
-# ACACIG
-Proyecto Comuntarias ESPOL para Asociación de Ciegos y Amigos de los Ciegos del Guayas
-
-#Ejecutar
-1. npm install -g nodemon
-2. nodemon index.js
--- backend/src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_PATH, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-})
